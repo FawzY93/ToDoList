65d82150d1f9ae7c02ba9a679f754203218d7a8a
Mohamed Hussein <a-mohuss@microsoft.com>
2017-02-02 14:39:36 +0200

    Simple add user page

---
+++ ToDoList/Scripts/User/Add/ValidateForm.js
@@ -0,0 +1,11 @@
+﻿function validateRequiredFields() {
+    name = $("#name").val();
+    age = $("#age").val();
+    id = $("#id").val();
+
+    if (!(name && age && id)) {
+        $("#message").text("Please provide all the required fields");
+        event.preventDefault();
+    }
+}
+
